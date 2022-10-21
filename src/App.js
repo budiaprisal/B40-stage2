4abc17eb3ec12a1a651ee33adcc049bbe850534a
budiaprisal <budiaprisal41@gmail.com>
2022-10-21 14:53:18 +0700

    chalangge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css'
 import './styles/styles.css'
-import Navigation from './component/Navigation'
+
 import Home from './pages/Home'
-import Menu from './component/Menu'
-import Profile from './pages/Profile'
-import Addproduk from './component/Addproduk'
-import EditProfile from './component/EditProfile'
-import Cardorder from './component/Cardorder'
-import IncomeTransaction from './component/IncomeTransaction'
-import ProfileUser from './component/ProfileUser'
-import EditUser from './component/EditUser'
+import Detail from './component/Detail'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -17,21 +11,9 @@ function App() {
   return (
     <div>
       <Router>
-        <Navigation />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/Menu" element={<Menu />} />
-          <Route exact path="/Profile" element={<Profile />} />
-          <Route exact path="/Addproduk" element={<Addproduk />} />
-          <Route exact path="/EditProfile" element={<EditProfile />} />
-          <Route exact path="/Cardorder" element={<Cardorder />} />
-          <Route exact path="/ProfileUser" element={<ProfileUser />} />
-          <Route exact path="/EditUser" element={<EditUser />} />
-          <Route
-            exact
-            path="/IncomeTransaction"
-            element={<IncomeTransaction />}
-          />
+          <Route exact path="/Detail" element={<Detail />} />
         </Routes>
       </Router>
     </div>
